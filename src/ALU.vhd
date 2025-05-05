f801e28c347be5278ca0bd7a504c8ec07e16915f
Zoginator <C27soren.herzog@afacademy.af.edu>
2025-05-05 17:08:40 -0600

    submission

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -32,11 +32,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- use UNISIM.VComponents.all;
  
 entity ALU is
-    Port ( A_in    : in  STD_LOGIC_VECTOR (7 downto 0);
-           B_in    : in  STD_LOGIC_VECTOR (7 downto 0);
-           ALUOp   : in  STD_LOGIC_VECTOR (2 downto 0);
-           result  : out STD_LOGIC_VECTOR (7 downto 0);
-           flags   : out STD_LOGIC_VECTOR (3 downto 0));
+    Port ( --A_in    : in  STD_LOGIC_VECTOR (7 downto 0);
+           --B_in    : in  STD_LOGIC_VECTOR (7 downto 0);
+           --ALUOp   : in  STD_LOGIC_VECTOR (2 downto 0);
+           --result  : out STD_LOGIC_VECTOR (7 downto 0);
+           --flags   : out STD_LOGIC_VECTOR (3 downto 0));
+           i_A : in STD_LOGIC_VECTOR (7 downto 0);
+           i_B : in STD_LOGIC_VECTOR (7 downto 0);
+           i_op : in STD_LOGIC_VECTOR (2 downto 0);
+           o_result : out STD_LOGIC_VECTOR (7 downto 0);
+           o_flags : out STD_LOGIC_VECTOR (3 downto 0));
 end ALU;
  
 architecture behavioral of ALU is
@@ -53,40 +58,40 @@ architecture behavioral of ALU is
     signal q_result : STD_LOGIC_VECTOR(7 downto 0);
 begin
     u0_ALU : ripple_adder
-        port map ( A    => A_in(3 downto 0),
-                   B    => B_in(3 downto 0),
+        port map ( A    => i_A(3 downto 0),
+                   B    => i_B(3 downto 0),
                    Cin  => '0',
                    S    => x_sum(3 downto 0),
                    Cout => x_lower_carry );
  
     Ripple_Upper : ripple_adder -- Upper Bits
-        port map ( A    => A_in(7 downto 4),
-                   B    => B_in(7 downto 4),
+        port map ( A    => i_A(7 downto 4),
+                   B    => i_B(7 downto 4),
                    Cin  => x_lower_carry,
                    S    => x_sum(7 downto 4),
                    Cout => x_upper_carry );
  
     -- MUX
-    resultOUT <= x_sum when ALUOp = "000" else
-                 x_sum when ALUOp = "001" else
-                 (others => '0') when ALUOp = "010" else
+    resultOUT <= x_sum when i_op = "000" else
+                 x_sum when i_op = "001" else
+                 (others => '0') when i_op = "010" else
                  (others => '0');
  
     q_result <= resultOUT;
  
     -- overflow flag
-    flags(0) <= not (A_in(7) xor B_in(7)) and (A_in(7) xor x_sum(7));
+    o_flags(0) <= not (i_A(7) xor i_B(7)) and (i_A(7) xor x_sum(7));
  
     -- carry
-    flags(1) <= x_upper_carry and (not ALUOp(0));
+    o_flags(1) <= x_upper_carry and (not i_op(0));
  
     -- negative
-    flags(2) <= resultOUT(7);
+    o_flags(2) <= resultOUT(7);
  
     -- zero
-    flags(3) <= '1' when (resultOUT = "00000000") else '0';
+    o_flags(3) <= '1' when (resultOUT = "00000000") else '0';
  
-    result <= q_result;
+    o_result <= q_result;
 end behavioral;
 
  
